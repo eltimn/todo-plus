83ae30cafb37ea0f4e74c84a2cc113c4d2671ecf
Tim Nelson <tim@eltimn.com>
2024-03-02 22:10:05 -0600

    Try building with gomod2nix

--- flake.nix
+++ flake.nix
@@ -13,9 +13,14 @@
       url = "github:hercules-ci/gitignore.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    gomod2nix = {
+      url = "github:nix-community/gomod2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, templ, gitignore }@inputs:
+  outputs = { self, nixpkgs, templ, gitignore, gomod2nix }@inputs:
     let
       templ = system: self.inputs.templ.packages.${system}.templ;
 
@@ -40,67 +45,101 @@
 
     in {
       packages = forAllSystems (system:
-        let pkgs = nixpkgsFor.${system};
+        let
+          pkgs = nixpkgsFor.${system};
+          buildGoApplication =
+            gomod2nix.legacyPackages.${system}.buildGoApplication;
         in {
-          web = pkgs.buildGo122Module {
-            pname = "web";
+          todo-plus = buildGoApplication {
             inherit version;
-            # version = "0.1.0";
+            name = "todo-plus";
             src = gitignore.lib.gitignoreSource ./.;
+            go = pkgs.go_1_22;
+            # Must be added due to bug https://github.com/nix-community/gomod2nix/issues/120
+            pwd = ./.;
+            # subPackages = [ "cmd/todo-plus" ];
+            CGO_ENABLED = 0;
+            flags = [ "-trimpath" ];
+            ldflags = [ "-s" "-w" "-extldflags -static" ];
 
-            # This hash locks the dependencies of this package. It is
-            # necessary because of how Go requires network access to resolve
-            # VCS.  See https://www.tweag.io/blog/2021-03-04-gomod2nix/ for
-            # details. Normally one can build with a fake sha256 and rely on native Go
-            # mechanisms to tell you what the hash should be or determine what
-            # it should be "out-of-band" with other tooling (eg. gomod2nix).
-            # To begin with it is recommended to set this, but one must
-            # remember to bump this hash when your dependencies change.
-            # vendorHash = pkgs.lib.fakeHash;
-            vendorHash = "sha256-6Bl7mtoU3GIdfgmTh8JEbOGeyLr/Cz/DVTJjC5824Ic=";
-
-            #
-            # go build -o ./bin/server main.go
-
-            # configurePhase = ''
-            #   runHook preConfigure
-
-            #   ${templ system}/bin/templ generate
-
-            #   runHook postConfigure
-            # '';
+            preBuild = ''
+              echo "Generating code with templ ..."
+              ${templ system}/bin/templ generate
+            '';
 
             buildPhase = ''
               runHook preBuild
 
-              echo "Building todo-plus..."
-              # can't run npm here because nix does not have internet access
-              # ${pkgs.nodejs}/bin/npm ci
-              # ${pkgs.tailwindcss}/bin/tailwindcss -i ./web/assets/css/main.css -o dist/assets/css/main.css
-
-              # module lookup disabled by GOPROXY=off
-              # go get github.com/a-h/templ
-
-              # cannot find module providing package github.com/a-h/templ: import lookup disabled by -mod=vendor
-              echo "Generating code with templ ..."
-              ${templ system}/bin/templ generate
               echo "Building go binary ..."
               go build -o ./bin/server -mod=mod main.go
 
               runHook postBuild
             '';
-
-            installPhase = ''
-              runHook preInstall
-
-              # mkdir -p $out/bin
-              # cp -r node_modules $out/node_modules
-              # cp package.json $out/package.json
-              # cp -r dist $out/dist
-
-              runHook postInstall
-            '';
           };
+
+          # web = pkgs.buildGo122Module {
+          #   pname = "web";
+          #   inherit version;
+          #   # version = "0.1.0";
+          #   src = gitignore.lib.gitignoreSource ./.;
+
+          #   # This hash locks the dependencies of this package. It is
+          #   # necessary because of how Go requires network access to resolve
+          #   # VCS.  See https://www.tweag.io/blog/2021-03-04-gomod2nix/ for
+          #   # details. Normally one can build with a fake sha256 and rely on native Go
+          #   # mechanisms to tell you what the hash should be or determine what
+          #   # it should be "out-of-band" with other tooling (eg. gomod2nix).
+          #   # To begin with it is recommended to set this, but one must
+          #   # remember to bump this hash when your dependencies change.
+          #   # vendorHash = pkgs.lib.fakeHash;
+          #   vendorHash = "sha256-FmiIYJAnf3DtrLLaC82i0I7ia+eqExLf463jlSbsCPg=";
+
+          #   #
+          #   # go build -o ./bin/server main.go
+
+          #   # configurePhase = ''
+          #   #   runHook preConfigure
+
+          #   #   ${templ system}/bin/templ generate
+
+          #   #   runHook postConfigure
+          #   # '';
+
+          #   preBuild = ''
+          #     echo "Generating code with templ ..."
+          #     ${templ system}/bin/templ generate
+          #   '';
+
+          #   buildPhase = ''
+          #     runHook preBuild
+
+          #     # echo "Building todo-plus..."
+          #     # can't run npm here because nix does not have internet access
+          #     # ${pkgs.nodejs}/bin/npm ci
+          #     # ${pkgs.tailwindcss}/bin/tailwindcss -i ./web/assets/css/main.css -o dist/assets/css/main.css
+
+          #     # module lookup disabled by GOPROXY=off
+          #     # go get github.com/a-h/templ
+
+          #     # cannot find module providing package github.com/a-h/templ: import lookup disabled by -mod=vendor
+
+          #     echo "Building go binary ..."
+          #     go build -o ./bin/server -mod=mod main.go
+
+          #     runHook postBuild
+          #   '';
+
+          #   installPhase = ''
+          #     runHook preInstall
+
+          #     # mkdir -p $out/bin
+          #     # cp -r node_modules $out/node_modules
+          #     # cp package.json $out/package.json
+          #     # cp -r dist $out/dist
+
+          #     runHook postInstall
+          #   '';
+          # };
         });
 
       # Add dependencies that are only needed for development
@@ -109,10 +148,11 @@
         in {
           default = pkgs.mkShell {
             buildInputs = with pkgs; [
-              go
+              go_1_22
               gopls
               gotools
               go-tools
+              gomod2nix.legacyPackages.${system}.gomod2nix
               (templ system)
               go-task
               nodejs
@@ -141,7 +181,8 @@
       # The default package for 'nix build'. This makes sense if the
       # flake provides only one package or there is a clear "main"
       # package.
-      defaultPackage = forAllSystems (system: self.packages.${system}.web);
+      defaultPackage =
+        forAllSystems (system: self.packages.${system}.todo-plus);
 
       # apps = forAllSystems (system: {
       #   default = {
